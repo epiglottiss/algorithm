989d99223bb70c43e14043d66dcd20e856fc157f
epiglottiss <iop245@naver.com>
2020-12-19 23:36:13 +0900

    BackTracking

---
+++ NandM(7).cpp
@@ -0,0 +1,51 @@
+#include <algorithm>
+#include <vector>
+#include<iostream>
+
+using namespace std;
+
+class NandM {
+    int totalNumCount;
+    int selNumCount;
+    vector<int> numbers;
+    vector<int> selNumbers;
+public:
+    NandM() {
+        cin >> totalNumCount;
+        cin >> selNumCount;
+        for (int i = 0; i < totalNumCount; i++)
+        {
+            int temp;
+            cin >> temp;
+            numbers.push_back(temp);
+        }
+ 
+        sort(numbers.begin(), numbers.end());
+        BackTrack();
+    }
+    void BackTrack() {
+        if (selNumbers.size() == selNumCount) {
+            PrintNumbers();
+            return;
+        }
+
+        for (int i = 0; i < numbers.size(); i++)
+        {
+            selNumbers.push_back(numbers[i]);
+            BackTrack();
+            selNumbers.pop_back();
+        }
+    }
+
+    void PrintNumbers() {
+        for (int i = 0; i < selNumCount; i++)
+        {
+            cout << selNumbers[i] << ' ';
+        }
+        cout << '\n';
+    }
+};
+
+int main() {
+    NandM aa;
+}
